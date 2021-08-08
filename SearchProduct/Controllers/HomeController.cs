c30e208e8e69a3cc46ac0acb18f7a83cf088fe5c
ashishlakheraeng <86763781+ashishlakheraeng@users.noreply.github.com>
2021-08-08 21:28:56 +0530

    Add project files.

---
+++ SearchProduct/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using LumenWorks.Framework.IO.Csv;
+using SearchProduct.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SearchProduct.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+           
+            return View();
+
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult AmazonProduct()
+        {
+            var data = csvReader();
+
+            return View(data);
+        }
+        public List<AmazonProduct> csvReader()
+        {
+            var csvTable = new DataTable();
+            using (var csvReader = new CsvReader(new StreamReader(System.IO.File.OpenRead(@"C:\Users\ashish\Desktop\Ashish\results.csv")), true))
+            {
+                csvTable.Load(csvReader);
+            }
+            List<AmazonProduct> AmazonProductList = new List<AmazonProduct>();
+            for (int i = 0; i < csvTable.Rows.Count; i++)
+            {
+                AmazonProductList.Add(new AmazonProduct { Description = csvTable.Rows[i][0].ToString(), Rating = csvTable.Rows[i][1].ToString(), 
+                    ReviewCount = csvTable.Rows[i][2].ToString() ,URL= csvTable.Rows[i][3].ToString()
+                });
+            }
+            return AmazonProductList;
+        }
+    }
+}
---
+++ SearchProduct/Models/AmazonProduct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SearchProduct.Models
+{
+    public class AmazonProduct
+    {
+        public string Description { get; set; }
+        public string Rating { get; set; }
+        public string ReviewCount{ get; set; }
+        public string URL{ get; set; }
+
+    }
+}
